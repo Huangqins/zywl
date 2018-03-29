3e77175757cc4bc776464567a5e07a7d0426e2d8
Huangqin <2244644279@qq.com>
2018-03-29 15:00:49 +0800

    提交修改

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,7 @@
 const getters = {
-    username: state => state.username,
-    true_name: state => state.true_name,
-    phone: state => state.phone
+    username: state => state.user.username,
+    true_name: state => state.user.true_name,
+    phone: state => state.user.phone
 }
 
 export default getters
