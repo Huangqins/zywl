a04f9ddc7ca6e7bd7b78ec8fa67f44f0a76ecfe9
huangqin <2244644279@qq.com>
2018-04-10 18:55:55 +0800

    提交修改

--- src/components/layout/panel.vue
+++ src/components/layout/panel.vue
@@ -1,25 +1,37 @@
 <template>
     <div :class="$style.panel">
-        <div :class="$style.panel-horn-left"></div>
-        <div :class="$style.panel-horn-right"></div>
+        <div :class="$style.panel_horn_left"></div>
+        <div :class="$style.panel_horn_right"></div>
+        <div :class="$style.panel_content_title"></div>
+        <slot/>
     </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      panel: "panel",
+      panel_horn_left: "panel-horn-left",
+      panel_horn_right: "panel-horn-right",
+      panel_content_title: "panel-content-title"
+    };
+  }
+};
 </script>
 <style lang="scss" module>
 @import "../../styles/layout.scss";
 .panel {
   @include panel($width: 400px,$height: 400px);
-  & .panel-horn-left {
-       @include panel($width: 400px,$height: 400px);
-  }
-  & .panel-horn-right {
-       @include panel($width: 400px,$height: 400px);
-  }
 }
-
-
+.panel_horn_left {
+  @include panel-horn-left;
+}
+.panel_horn_right {
+  @include panel-horn-right;
+}
+.panel_content_title {
+  @include panel-content-title;
+}
 </style>
 
 
