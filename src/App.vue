d6b5ff6ec162be5e052a4580bab89c845841675b
wjj <3197612935@qq.com>
2018-05-03 15:46:07 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
         <headers v-if="token"></headers>
-　　　　<router-view  :style="{marginTop: token ? '60px': 0}"></router-view>
+　　　　<router-view  :style="{marginTop: token ? '36px': 0}"></router-view>
     </div>
 </template>
 <script>
