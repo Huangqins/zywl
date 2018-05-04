805adbf5c91e3209f393cd4fc2f5e054545437e3
wjj <3197612935@qq.com>
2018-05-04 10:38:18 +0800

    提交

--- src/components/guide/assetSet.vue
+++ src/components/guide/assetSet.vue
@@ -171,11 +171,11 @@ export default {
             }
           }
         },
-        // {
-        //   title: "创建人",
-        //   key: "target_tast_oper",
-        //   align: "center"
-        // },
+        {
+          title: "报告名称",
+          key: "target_reportname",
+          align: "center"
+        },
         {
           title: "操作",
           align: "center",
