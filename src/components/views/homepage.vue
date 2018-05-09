33508f6beb2185956bd75dd1e60794d3fb7cb89c
wjj <3197612935@qq.com>
2018-05-09 20:25:09 +0800

    提交

--- src/components/views/homepage.vue
+++ src/components/views/homepage.vue
@@ -54,7 +54,7 @@
                
                 <section>
                     <!-- <span class="Aipicture_text">外部安全资源</span> -->
-                    <Table :columns="assets" :data="assetsData" :height="200" ></Table>
+                    <chart :option="vulntypePic" width="550px" height="350px" id="vulnPic"></chart>
                </section>
                 <section>
                     <!-- <span class="Aipicture_text">主机风险情况</span> -->
@@ -70,7 +70,8 @@ import chart from "components/chart/chart";
 import assetsInfo from "api/assetsInfo";
 import { getUserName } from "@/utils/auth";
 import leaksInfo from "api/leaksInfo";
-import vulnTop from "api/vulnTop"
+import vulnTop from "api/vulnTop";
+import vulntype from "api/vulntype";
 export default {
   components: {
     force,
@@ -78,6 +79,44 @@ export default {
   },
   data() {
     return {
+      vulntypePic: {
+        tooltip: {},
+        legend: {
+          type: "scroll",
+          orient: "vertical",
+          right: 5,
+          top: 20,
+          bottom: 20,
+          data: []
+        },
+        radar: {
+          // shape: 'circle',
+          name: {
+            textStyle: {
+              color: "#fff",
+              backgroundColor: "#999",
+              borderRadius: 3,
+              padding: [3, 5]
+            }
+          },
+          indicator: [
+          {}
+          ]
+        },
+        series: [
+          {
+            name: "预算 vs 开销（Budget vs spending）",
+            type: "radar",
+            // areaStyle: {normal: {}},
+            data: [
+              {
+                value: [],
+                name: "风险总数分布雷达图"
+              }
+            ]
+          }
+        ]
+      },
       assets: [
         {
           title: "资产名称",
@@ -96,34 +135,34 @@ export default {
           key: "vuln_use"
         }
       ],
-      assetsData: [ ],
-      vulns:[
+      assetsData: [],
+      vulns: [
         {
           title: "风险名称",
           key: "vuln_name",
-          align:'center'
+          align: "center"
         },
         {
           title: "风险等级",
           key: "vuln_level",
-           align:'center'
+          align: "center"
         },
         {
           title: "风险利用情况",
           key: "vuln_useInfo",
-          align:'center'
+          align: "center"
         }
       ],
-      vulnsData:[],
+      vulnsData: [],
       width: "576px",
       widths: 300,
       options: {
         xAxis: {
           type: "category",
           data: [],
-          axisLabel: {           
-            formatter: function (item) {
-                //  return item
+          axisLabel: {
+            formatter: function(item) {
+              //  return item
             },
             textStyle: {
               color: "#CCCCCC"
@@ -139,7 +178,7 @@ export default {
           }
         },
         tooltip: {
-          trigger: 'axis',
+          trigger: "axis"
         },
         series: [
           {
@@ -148,54 +187,77 @@ export default {
           }
         ]
       },
-       defaultPage: {
+      defaultPage: {
         area: 1,
         rows: 10,
         page: 1,
         userName: getUserName()
       },
-     //top10排行榜
-      holes: [ ],
+      //top10排行榜
+      holes: []
     };
   },
-  mounted(){
-    this.assetsInfo(this.defaultPage)
-    this.leaksInfo()
-    this.vulntop()
+  mounted() {
+    this.assetsInfo(this.defaultPage);
+    this.leaksInfo();
+    this.vulntop();
+    this.vulntype();
   },
-  methods:{
+  methods: {
     //资产列表
-    assetsInfo(params){
-     assetsInfo(params).then(res=>{      
-       let data=res.rows
-       this.assetsData=data
-       this.assetsData.forEach(item =>{
-         this.options.xAxis.data.push(item.assets_name)
-         this.options.series[0].data.push(item.vuln_use)
-       })
-
-
-     }    
-    )},
-    //风险列表
-    leaksInfo(){
-      let params={userName:getUserName()}
-     leaksInfo(params).then(res=>{
-       let data=res.rows
-       this.vulnsData=data
-     }    
-    )},
-    //top10排行榜
-    vulntop(){
-      const params={ }
-     vulnTop(params).then(res => {
-        let data=res.lists
-       this.holes=data
-     }) 
+    assetsInfo(params) {
+      assetsInfo(params).then(res => {
+        let data = res.rows;
+        this.assetsData = data;
+        this.assetsData.forEach(item => {
+          this.options.xAxis.data.push(item.assets_name);
+          this.options.series[0].data.push(item.vuln_use);
+        });
+      });
     },
-
+    //风险列表
+    leaksInfo() {
+      let params = { userName: getUserName() };
+      leaksInfo(params).then(res => {
+        let data = res.rows;
+        this.vulnsData = data;
+      });
+    },
+    //top10排行榜
+    vulntop() {
+      const params = {};
+      vulnTop(params).then(res => {
+        let data = res.lists;
+        this.holes = data;
+      });
+    },
+    //风险类型
+    vulntype() {
+      let params = { flag: 1 };
+      // this.$refs.vulntypePic.showLoading();
+      vulntype(params).then(res => {
+        if (res.result === 0) {
+          // this.$refs.vulntype.hideLoading();
+          let list = res.list;
+          let numList = [];
+           this.vulntypePic.radar.indicator = []
+          numList = list.map(item => {
+            return Number(item.kb_vuln_vnum);
+          });
+          list.forEach(item => {
+            this.vulntypePic.legend.data.push(item.vuln_type_name);
+            this.vulntypePic.series[0].data[0].value.push(
+              Number(item.kb_vuln_vnum)
+            );
+            this.vulntypePic.radar.indicator.push({
+              max: Math.max.apply(null, numList),
+              name: item.vuln_type_name
+            });
+          });
+        }
+      });
+    }
   }
-
 };
 </script>
 <style scoped>
@@ -219,7 +281,7 @@ export default {
   border: 1px solid #2b4e6f;
 }
 .preload {
-  width:100%;
+  width: 100%;
   margin-top: 10px;
   border: 1px solid #2b4e6f;
 }
@@ -231,7 +293,7 @@ export default {
   margin: 0px 0px 10px 0px;
   box-sizing: border-box;
 }
-.secThree section{
+.secThree section {
   margin-top: 10px;
   border: 1px solid #2b4e6f;
 }
@@ -284,8 +346,8 @@ export default {
 }
 /* top10排行榜样式 header*/
 .secThree span {
-    display: inline-block;
-    height: 19px;
+  display: inline-block;
+  height: 19px;
 }
 /* top10排行榜样式 end*/
 </style>
